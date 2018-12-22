c90e126ec416abb305a33e109e5b4a7ac557ad92
lobcus <lobcus@gmail.com>
2018-12-22 17:22:47 -0500

    Removed sql

--- XMas-CTF 2018/Santa's Lucky Number/brute.py
+++ XMas-CTF 2018/Santa's Lucky Number/brute.py
@@ -1,9 +1,10 @@
 import urllib.request
+import urllib.error
 
 WEBSITE = "http://199.247.6.180:12005/?page="
 
 
-def iterative():
+def find_xmas():
     x = 0
     while True:
         try:
@@ -11,9 +12,9 @@ def iterative():
             ps = html.read().decode("utf-8")
             b = ps.find("<p align=\"center\">", ps.find("<p align=\"center\">") + 1)
             e = ps.find("</p>", ps.find("</p>") + 1)
-            STRING = str(ps[b + 19:e])
-            if STRING.find("X-MAS") != -1:
-                print(STRING)
+            val = str(ps[b + 19:e])
+            if val.find("X-MAS") != -1:
+                print(val)
                 break
         except urllib.error.URLError as err:
             print(err)
@@ -21,7 +22,7 @@ def iterative():
 
 
 def main():
-    iterative()
+    find_xmas()
 
 
 main()
